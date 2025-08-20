508af553e18eff6564251ab3a259ea0c9858b950
ji-mim <194115@jnu.ac.kr>
2025-08-21 00:40:17 +0900

    project setting

---
+++ src/main/java/com/sorion/SorionApplication.java
@@ -0,0 +1,13 @@
+package com.sorion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sorion/SorionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
