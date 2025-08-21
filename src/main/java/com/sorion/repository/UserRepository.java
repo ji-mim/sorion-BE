c1f1712f3a96c3c1765a41e83423c5649a7d48bc
ji-mim <194115@jnu.ac.kr>
2025-08-22 01:21:01 +0900

    feat: add method

--- src/main/java/com/sorion/repository/UserRepository.java
+++ src/main/java/com/sorion/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.sorion.repository;
 import com.sorion.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+
 }
