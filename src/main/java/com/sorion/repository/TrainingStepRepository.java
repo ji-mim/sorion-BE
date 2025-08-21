c6500bd44f918a5fd08e764d2874c0a08ffc3886
ji-mim <194115@jnu.ac.kr>
2025-08-21 21:40:39 +0900

    refactor: add method

--- src/main/java/com/sorion/repository/TrainingStepRepository.java
+++ src/main/java/com/sorion/repository/TrainingStepRepository.java
@@ -1,7 +1,11 @@
 package com.sorion.repository;
 
+import com.sorion.domain.Training;
 import com.sorion.domain.TrainingStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TrainingStepRepository extends JpaRepository<TrainingStep, Long> {
+    List<TrainingStep> findByTraining(Training training);
 }
