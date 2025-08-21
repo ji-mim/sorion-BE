e4921ee670d82e0a3fe9184792bacbb8a75d7d6b
ji-mim <194115@jnu.ac.kr>
2025-08-22 00:33:15 +0900

    feat: add method

--- src/main/java/com/sorion/repository/SoundRepository.java
+++ src/main/java/com/sorion/repository/SoundRepository.java
@@ -1,7 +1,11 @@
 package com.sorion.repository;
 
+import com.sorion.domain.Category;
 import com.sorion.domain.Sound;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SoundRepository extends JpaRepository<Sound, Long> {
+    List<Sound> findByCategory(Category category);
 }
